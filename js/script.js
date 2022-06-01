c5c2ad02ed5aa8679607fa0f555a4d60acdb30f9
Neeraj Kumar <neerajkumarsingh1999@gmail.com.com>
2022-06-01 15:05:34 +0530

    My first File

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+const passSecure =() =>{
+    const passAdmin="notmebutyou";
+    const passCode= window.prompt("Enter Passcode");
+    if (passCode==passAdmin){
+        window.alert("Welcome Admin, Jai Hind");
+    }
+    else {
+        window.location="https://www.instagram.com/_neeraj_dev_/";
+    }
+}
+passSecure();
+
+
+
